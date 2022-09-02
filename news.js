248314f734680079226f2db808c32205db296641
meraj2085 <samirmeraj60@gmail.com>
2022-09-02 22:21:38 +0600

    loading catagories

--- news.js
+++ news.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const loadCatagories = async () =>{
+     url = `https://openapi.programming-hero.com/api/news/categories`;
+     const res = await fetch(url);
+     const data = await res.json()
+     displayData(data.data.news_category)
+}
+
+const displayData = (catagories) =>{
+     console.log(catagories)
+}
+
+loadCatagories()
