efc6c7f9149ee841772def2c7764048abc51b89a
meraj2085 <samirmeraj60@gmail.com>
2022-09-02 23:49:54 +0600

    adding blog

--- news.js
+++ news.js
@@ -20,3 +20,35 @@ const displayData = (catagories) =>{
 }
 
 loadCatagories()
+
+//========================================//======================================//
+
+const loadNews = async () =>{
+     const url = `https://openapi.programming-hero.com/api/news/category/01`
+     const res = await fetch(url);
+     const data = await res.json();
+     displayNews(data.data)
+}
+
+const displayNews = (newses) =>{
+     const newsContainer = document.getElementById('news-card-container');
+     newses.forEach(news => {
+          const {author, thumbnail_url, image_url, details, title} = news;
+
+          console.log(news)
+          const cardCol = document.createElement('div');
+          cardCol.classList.add('col');
+          cardCol.innerHTML = `
+          <div class="card h-100">
+          <img src="${image_url}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${title}</h5>
+            <p class="card-text">${details.length > 100 ? details.slice(0,100) + '...' : details}</p>
+          </div>
+          </div>
+          `
+          newsContainer.appendChild(cardCol);
+     });
+}
+
+loadNews()
