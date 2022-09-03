8fd2de58b11dd0fd589f501e408a17d49aeaef58
meraj2085 <samirmeraj60@gmail.com>
2022-09-03 07:53:23 +0600

    adding spinner

--- news.js
+++ news.js
@@ -15,7 +15,7 @@ const displayData = (catagories) =>{
      catagories.forEach(category => {
           const categoryList = document.createElement('div');
           categoryList.innerHTML = `
-               <h5 onclick="loadNews(${category.category_id})">${category.category_name}</h5>
+               <h5 onclick="loadNews('${category.category_id}'), spinner(true)">${category.category_name}</h5>
           `
           catagoriesContainer.appendChild(categoryList)
      });
@@ -25,54 +25,73 @@ loadCatagories()
 
 //========================================//======================================//
 
-const loadNews = async () =>{
-     const url = `https://openapi.programming-hero.com/api/news/category/01`
+const loadNews = async (id) =>{
+     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
      const res = await fetch(url);
      const data = await res.json();
      displayNews(data.data)
 }
 
 const displayNews = (newses) =>{
-     const newsContainer = document.getElementById('news-card-container');
-     newses.forEach(news => {
-          const {author,rating, thumbnail_url, image_url, details, title} = news;
-          const {name, img, published_date} = author;
+     const resultContainer = document.getElementById('result-numbers');
+     resultContainer.innerHTML = `
+          <h5>${newses.length} items found for this category.</h5>
+          `
 
-          console.log(news)
-          const cardDiv = document.createElement('div');
-          cardDiv.classList.add('col');
-          cardDiv.innerHTML = `
-          <div class="card mb-3" style="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
+          const newsContainer = document.getElementById('news-card-container');
+          newsContainer.textContent = '';
+          
+          newses.forEach(news => {
+               const {author,rating, thumbnail_url, image_url, details, title} = news;
+               const {name, img, published_date} = author;
+
+               const cardDiv = document.createElement('div');
+               cardDiv.classList.add('col');
+               cardDiv.innerHTML = `
+               <div class="card mb-3" style="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;">
+               <div class="row g-0">
+               <div class="col-md-4">
+               <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
               <div class="card-body">
                 <h5 class="card-title">${title}</h5>
                 <p style="color: #000000dd;" class="card-text">${details.length > 200 ? details.slice(0,200) + '...' : details}</p>
                 <div class="d-flex justify-content-between">
-                    <div class="d-flex">
-                         <div class="me-2" style="width: 30px;">
-                         <img class="img-fluid rounded-circle" src="${img}" alt="">
+                <div class="d-flex">
+                <div class="me-2" style="width: 30px;">
+                <img class="img-fluid rounded-circle" src="${img}" alt="">
                          </div>
                          <p class="mb-0">${name}</p>
-                    </div>
-                    <div class="fs-5 d-flex">
+                         </div>
+                         <div class="fs-5 d-flex">
                <p class="me-2"><i class="fa-sharp fa-solid fa-eye"></i></p>
-              <p>${rating.number}</p>
+               <p>${rating.number}</p>
                </div>
                <div>
-              <i style="color: #5D5FEF; font-size: 25px; margin-top: 3px;" class="fa-solid fa-arrow-right"></i>
+               <i style="color: #5D5FEF; font-size: 25px; margin-top: 3px;" class="fa-solid fa-arrow-right"></i>
                </div>
                </div>
-              </div>
+               </div>
             </div>
-          </div>
-          </div>
-          `
-          newsContainer.appendChild(cardDiv)
-     });
-}
+            </div>
+            </div>
+            `
+            newsContainer.appendChild(cardDiv)
+          });
+          spinner(false)
+     }
+     
+     loadNews('08');
 
-loadNews()
+//========================================//======================================//
+
+const spinner = (isLoading) =>{
+     const toggleSpinner = document.getElementById('spinner');
+     if(isLoading === true){
+          toggleSpinner.classList.remove('d-none')
+     }
+     else{
+          toggleSpinner.classList.add('d-none');
+     }
+}
---
+++ test.js
@@ -0,0 +1,2 @@
+const numbers = [{a:1}, {b:2}, {c:3}, {d:4}];
+console.log(numbers.length)
